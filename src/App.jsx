5587581983d1cbe37a1bd76ad546687db0fef049
vapppu <veera.hiltunen@gmail.com>
2023-10-05 12:23:16 +0300

    Check if person already exists in phonebook before adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
-  ])
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
-    setPersons(persons.concat({name: newName}))
-  }
+    const newPerson = { name: newName };
+    if (!persons.find((person) => person.name === newPerson.name)) {
+      setPersons(persons.concat(newPerson))
+      setNewName("")
+
+      console.log("New person added")
+    }
+    else {
+      console.log("Person found and therefore not added")
+    }
+    console.log("Setting new name to empty")
+    setNewName("")
+  };
 
   const handleChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input onChange={handleChange}/>
+          name: <input onChange={handleChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-      {persons.map((person) => <li key={person.name}>{person.name}</li>)}
+      <ul style={{ listStyle: "none" }}>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
