a02afa05551d62bdc13fc06a3f04c578693be914
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-01 19:59:41 +0200

    If person is already in book with a different number, ask user if user wants to update number to the new one

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,112 @@
 import { useState, useEffect } from "react";
 
-import AddPerson from './components/AddPerson'
-import ListPersons from './components/ListPersons'
-import FilterForm from './components/FilterForm'
-import personService from './services/persons'
+import AddPerson from "./components/AddPerson";
+import ListPersons from "./components/ListPersons";
+import FilterForm from "./components/FilterForm";
+import personService from "./services/persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [filteredPersons, setFilteredPersons] = useState(persons)
+  const [persons, setPersons] = useState([]);
+  const [filteredPersons, setFilteredPersons] = useState(persons);
   const [searchTerm, setSearchTerm] = useState("");
 
-  const personDB = 'http://localhost:3001/persons'
+  const personDB = "http://localhost:3001/persons";
 
   useEffect(() => {
-    console.log("Fetch persons")
-    personService
-      .getAll()
-      .then((initialPersons) => {
-        setPersons(initialPersons)
-      })
-  }, [])
+    console.log("Fetch persons");
+    personService.getAll().then((initialPersons) => {
+      setPersons(initialPersons);
+    });
+  }, []);
 
   useEffect(() => {
-    setFilteredPersons(persons)
-  }, [persons])
+    setFilteredPersons(persons);
+  }, [persons]);
 
   const filterPersons = (substring) => {
-    const filtered = persons.filter((person) => person.name.toLowerCase().includes(substring.toLowerCase()))
-    setFilteredPersons(filtered)
-  }
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(substring.toLowerCase())
+    );
+    setFilteredPersons(filtered);
+  };
 
   const addPerson = (person) => {
-    if (!persons.find((personInList) => personInList.name.toLowerCase() === person.name.toLowerCase())) {
-      personService
-        .create(person)
-        .then(returnedPerson => {
-          console.log(returnedPerson)
-          const newPersons = persons.concat(returnedPerson)
-          setPersons(newPersons)
-          setFilteredPersons(newPersons)
-        })
-    }
 
-    else {
-      alert(`${person.name} is already added in the phone book`)
+    const personInBook = persons.find(
+      (personInList) =>
+        personInList.name.toLowerCase() === person.name.toLowerCase()
+    )
+
+    if (!personInBook) {
+      personService.create(person).then((returnedPerson) => {
+        console.log(returnedPerson);
+        const newPersons = persons.concat(returnedPerson);
+        setPersons(newPersons);
+        setFilteredPersons(newPersons);
+      });
+    } 
+    else if (personInBook.number === person.number) {
+      alert(`${personInBook.name} is already added in the phone book`);
+      }
+      
+      else if (window.confirm(`Person ${person.name} is already added in the phone book. Replace old number with new one?`))
+        {        
+          personService
+          .update(personInBook.id, person)
+          .then((response) =>  {
+            const newPersons = persons.map((person) => (person.id !== response.id ? person : response))
+            setPersons(newPersons)
+            setFilteredPersons(newPersons)
+            })    
+        
     }
-  }
+  
+};
 
   const removePerson = (person) => {
-    console.log(`Button clicked to remove person ${person.name}`)
-    const personToRemove = persons.find((personInList) => personInList.id === person.id)
-    if (personToRemove && (window.confirm(`Do you really want to delete ${personToRemove.name}`))) {
+    console.log(`Button clicked to remove person ${person.name}`);
+    const personToRemove = persons.find(
+      (personInList) => personInList.id === person.id
+    );
+    if (
+      personToRemove &&
+      window.confirm(`Do you really want to delete ${personToRemove.name}?`)
+    ) {
       personService
-      .remove(personToRemove.id)
-      .then((response) => {
-        const newPersons = persons.filter(person => person.id !== personToRemove.id)
-        setPersons(newPersons)
-        setFilteredPersons(newPersons)
-      })
-      .catch(error => {
-        alert(`Person ${person.name} was already deleted from server`)
-        setPersons(persons.filter(person => person.id !== personToRemove.id))
-      })
+        .remove(personToRemove.id)
+        .then((response) => {
+          const newPersons = persons.filter(
+            (person) => person.id !== personToRemove.id
+          );
+          setPersons(newPersons);
+          setFilteredPersons(newPersons);
+        })
+        .catch((error) => {
+          alert(`Person ${person.name} was already deleted from server`);
+          setPersons(
+            persons.filter((person) => person.id !== personToRemove.id)
+          );
+        });
     }
-  }
+  };
 
   const updateSearchTerm = (string) => {
-    setSearchTerm(string)
-  }
-
+    setSearchTerm(string);
+  };
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <FilterForm filterPersons={filterPersons} searchTerm = {searchTerm} updateSearchTerm = {updateSearchTerm}/>
-      <AddPerson persons = {persons} addPerson = {addPerson}/>
-      <ListPersons filteredPersons = {filteredPersons} removePerson = {removePerson}/>
+      <FilterForm
+        filterPersons={filterPersons}
+        searchTerm={searchTerm}
+        updateSearchTerm={updateSearchTerm}
+      />
+      <AddPerson persons={persons} addPerson={addPerson} />
+      <ListPersons
+        filteredPersons={filteredPersons}
+        removePerson={removePerson}
+      />
     </div>
   );
 };
